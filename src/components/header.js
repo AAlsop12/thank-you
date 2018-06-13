3a45c14700242bb2c19b138a0adcc82f0c531cd7
AAslsop12 <aimee.johansen33@gmail.com>
2018-06-12 20:11:15 -0600

    Built out the header component

---
+++ src/components/header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+const Header = () => {
+    return (
+        <div classname="header">
+            <h1>Thank You Card</h1>
+            <p>Dear ().  Thank you so much for the ().  I really () it .  I appreciate your () gesture.  Love, ().</p>
+        
+        </div>
+    )
+}
+
+export default Header;
