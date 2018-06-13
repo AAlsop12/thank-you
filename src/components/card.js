4a973d7de8d968a3d80d3bb2bf237b9e6bf23814
AAslsop12 <aimee.johansen33@gmail.com>
2018-06-12 21:53:49 -0600

    started to implement state in the card component

--- src/components/card.js
+++ src/components/card.js
@@ -3,9 +3,35 @@ import React, { Component } from 'react';
 import Input from './input';
 
 class ThankYouCard extends Component {
+
+    constructor() {
+        super()
+
+        this.state = {
+            nameOfRecipient: 'Orange',
+            item: ''
+
+            
+        }
+    }
+
+    handleInputChange() {
+        this.setState({ nameOfRecipient: 'Leslie'})
+    }
+
     render() {
+
         return (
             <div className="thank_you_card">
+                <h1>{this.state.nameOfRecipient}</h1>
+                <input/>
+            </div>
+
+        )
+
+        return (
+            <div className="thank_you_card">
+                <h1>{this.state.nameOfRecipient}</h1>
                 { Input('Name of Recipient') }
                 { Input('Item') }
                 { Input('Positive Emotion') }
